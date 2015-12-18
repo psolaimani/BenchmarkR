e533a08fe2ad2300b58e095f69ee5895a5fba1e3
Parham Solaimani <parham@bedatadriven.com>
2015-12-18 14:07:38 +0100

    added function to extract used library/package names in input script

--- R/Getters.R
+++ R/Getters.R
@@ -1,8 +1,8 @@
 getId <- function(){
   # Generated a unique ID for each benchmark run using date, time, and a random number
-  # returns String '8 digit date + . + 6 digit time + . + 4 digit random number'
-  exactTime <- format(Sys.time(), "%C%g%m%d.%H%M%S.")
-  randomNum <- sample(1000:9999, 1)
+  # returns String '8 digit date + . + 6 digit time + . + 2 digit random number'
+  exactTime <- format(Sys.time(), "%Y%m%d.%H%M%S.")
+  randomNum <- sample(10:99, 1)
   id <- paste0(exactTime,randomNum)
   return(id)
 }
@@ -36,4 +36,25 @@ getWarnings <- function(){
 getSystemID <- function(){
   # get the unique systemId that is used to identify this system
   return(ExecEnvironment$META$systemId[1])
+}
+
+getUsedLibs <- function(file){
+  # Identifies all used libraries within input file/script
+  # detect all library("package") or require("package"), and
+  # extract the package names
+  require(stringr)
+  lines <- readLines(file)
+  # make a vector of lines with library( or require(
+  usedLibrary <- as.vector(
+    na.omit(str_extract(lines, "(library|require)\\(.*"))
+  )
+  extractLibrary <- c(character(0))
+  for(i in 1:length(usedLibrary)){
+    # extract the package name from library(package) and remove
+    # the optional ' or " characters around package name
+    currentLib <- sub("(library|require)\\({1}(\"|\')*([[:alnum:]]*)(\"|\')*\\){1}",
+                      "\\3", usedLibrary[i], perl = TRUE)
+    extractLibrary <- c(extractLibrary,currentLib)
+  }
+  return(extractLibrary)
 }
--- test/test.R
+++ test/test.R
@@ -3,11 +3,19 @@ for(i in 1:100){
   rnorm(100)
 }
 
+library(utils)
+
 utils::read.table("/home/parham/workspace/benchmaRk/test/RNAseq2.txt",sep="\t",dec=".")
+
+library("parallel")
 
 for(i in 1:200){
   rnorm(200)
 }
+
+require(plyr)
+
+require('cluster')
 
 read.table("/home/parham/workspace/benchmaRk/test/RNAseq2.txt",sep="\t",dec=".")
 

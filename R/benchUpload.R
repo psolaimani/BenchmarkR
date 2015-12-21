0a3cb548fc302c89faf118845464954247b988a8
Parham Solaimani <parham@bedatadriven.com>
2015-12-21 12:43:47 +0100

    starting with upload to database functionality.

---
+++ R/benchUpload.R
@@ -0,0 +1,8 @@
+# benchUpload(info=c(TIMINGS, META, BENCHMARKS),type="DB",con=c(user,pass,url))
+# allows upload of timings/benchmarks/meta data to database
+
+benchUpload <- function(info,type="DB",con){
+
+
+  cat("\n\nDATA uploaded to database.\n")
+}

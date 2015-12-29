3c4c268aab9548d588a494b01546283f4eaab814
Parham Solaimani <parham@bedatadriven.com>
2015-12-29 13:06:43 +0100

    fixed example usages and added data.table fread

--- test/example_usage.R
+++ test/example_usage.R
@@ -2,31 +2,31 @@ timed_functions <- data.frame(
   timed_fun = c(
     "write.table","write.csv","write.csv2","write.delim2",
     "read.table","read.csv","read.csv2","read.delim","read.delim2",
-    "download.file","curlGetHeaders"
+    "download.file","curlGetHeaders", "fread"
     ),
   package = c(
     "utils","utils","utils","utils","utils","utils",
-    "utils","utils","utils", "utils", "base"
+    "utils","utils","utils", "utils", "base", "data.table"
     ),
   category = c(
     "WRITE","WRITE","WRITE","WRITE","READ","READ",
-    "READ","READ","READ", "DOWNLOAD", "DOWNLOAD"
+    "READ","READ","READ", "DOWNLOAD", "DOWNLOAD", "READ"
     ),
   type = c(
-    "IO","IO","IO","IO","IO","IO",
-    "IO","IO","IO","IO","IO"
+    "IO", "IO", "IO", "IO", "IO", "IO",
+    "IO", "IO", "IO", "IO", "IO", "IO"
   ),
   stringsAsFactors=FALSE
 )
 
-benchmarkSource("test/test.R",timed_functions = timed_functions)
+benchmarkSource(file = "test/test.R",timed_functions = timed_functions)
 
-benchmarkSource("test/test2.R",timed_functions = timed_functions)
+benchmarkSource(file = "test/test2.R",timed_functions = timed_functions)
 
-benchmarkSource("test/test.R")
+benchmarkSource(file = "test/test.R")
 
-benchmarkSource("test/test2.R")
+benchmarkSource(file = "test/test2.R")
 
-benchGetter("alltimings")
+benchGetter("allprofiles")
 
 benchGetter("allbenchmarks")

ea6262dcfb8b453f8a1a319a2cfc2bc49df7291c
Parham Solaimani <parham@bedatadriven.com>
2016-01-05 12:45:15 +0100

    files created during test will be removed

--- tests/test2.R
+++ tests/test2.R
@@ -14,9 +14,10 @@ x <- read.table("/home/parham/workspace/benchmaRk/test/RNAseq2.txt",sep="\t",dec
 y <- read.csv("/home/parham/workspace/benchmaRk/test/RNAseq2.txt")
 
 write.csv2(y, "/home/parham/workspace/benchmaRk/test/RNAseq2.csv2")
+file.remove("/home/parham/workspace/benchmaRk/test/RNAseq2.csv2")
 
 write.table(x, "/home/parham/workspace/benchmaRk/test/RNAseq2.table")
-
+file.remove("/home/parham/workspace/benchmaRk/test/RNAseq2.table")
 
 for(i in 1:1000){
   rnorm(500)

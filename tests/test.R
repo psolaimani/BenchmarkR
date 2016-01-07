6ef43d71bcc13f5a28112f5d2ec25936cc41d00b
Parham Solaimani <parham@bedatadriven.com>
2016-01-07 01:55:54 +0100

    used data(RNAseq2) instead of ../data/RNAseq2.txt

--- tests/test.R
+++ tests/test.R
@@ -2,7 +2,7 @@ for(i in 1:100){
   rnorm(100)
 }
 
-dt <- utils::read.table("../data/RNAseq2.txt",sep="\t",dec=".")
+dt <- utils::read.table(data(RNAseq2),sep="\t",dec=".")
 head(dt)[1,1]
 tail(dt)[1,1]
 
@@ -10,23 +10,23 @@ for(i in 1:200){
   rnorm(200)
 }
 
-dt <- read.table("../data/RNAseq2.txt",sep="\t",dec=".")
+dt <- read.table(data(RNAseq2),sep="\t",dec=".")
 head(dt)[1,1]
 tail(dt)[1,1]
 
-dt <- read.csv("../data/RNAseq2.txt",sep="\t")
+dt <- read.csv(data(RNAseq2),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
 
-dt <- read.csv2("../data/RNAseq2.txt",sep="\t")
+dt <- read.csv2(data(RNAseq2),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
 
-dt <- utils::read.delim("../data/RNAseq2.txt",sep="\t")
+dt <- utils::read.delim(data(RNAseq2),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
 
-dt <- read.delim2("../data/RNAseq2.txt",sep="\t")
+dt <- read.delim2(data(RNAseq2),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
 

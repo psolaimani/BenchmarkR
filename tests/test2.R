65b2970d3eaa227d764beeb3b073223c591c4bab
Parham Solaimani <parham@bedatadriven.com>
2016-01-12 10:49:02 +0100

    commented out environment tests

--- tests/test2.R
+++ tests/test2.R
@@ -2,18 +2,18 @@ library("hamcrest")
 require("benchmarkR")
 
 
-assertThat(class(benchmarkR:::BenchmarkEnvironment), "environment")
-assertThat(class(benchmarkR:::ExecEnvironment), "environment")
-assertThat(class(benchmarkR:::ExecEnvironment$PROFILES), "data.frame")
-assertThat(class(benchmarkR:::ExecEnvironment$BENCHMARKS), "data.frame")
-assertThat(class(benchmarkR:::ExecEnvironment$META), "data.frame")
-assertThat(class(benchmarkR:::ExecEnvironment$WARNINGS), "data.frame")
+#assertThat(class(benchmarkR:::BenchmarkEnvironment), "environment")
+#assertThat(class(benchmarkR:::ExecEnvironment), "environment")
+#assertThat(class(benchmarkR:::ExecEnvironment$PROFILES), "data.frame")
+#assertThat(class(benchmarkR:::ExecEnvironment$BENCHMARKS), "data.frame")
+#assertThat(class(benchmarkR:::ExecEnvironment$META), "data.frame")
+#assertThat(class(benchmarkR:::ExecEnvironment$WARNINGS), "data.frame")
 
 
-assertThat(ncol(benchmarkR:::ExecEnvironment$PROFILES), 7)
-assertThat(ncol(benchmarkR:::ExecEnvironment$BENCHMARKS), 4)
-assertThat(ncol(benchmarkR:::ExecEnvironment$META), 3)
-assertThat(ncol(benchmarkR:::ExecEnvironment$WARNINGS), 3)
+#assertThat(ncol(benchmarkR:::ExecEnvironment$PROFILES), 7)
+#assertThat(ncol(benchmarkR:::ExecEnvironment$BENCHMARKS), 4)
+#assertThat(ncol(benchmarkR:::ExecEnvironment$META), 3)
+#assertThat(ncol(benchmarkR:::ExecEnvironment$WARNINGS), 3)
 
 assertThat(addProfiler(), NULL)
 dt <- data.frame(a=c(1,2),b=c(1,2))

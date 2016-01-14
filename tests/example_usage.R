31e835b542763685ce0970e26a47288a544116be
Parham Solaimani <parham@bedatadriven.com>
2016-01-14 21:39:15 +0100

    extend test units

--- tests/example_usage.R
+++ tests/example_usage.R
@@ -25,3 +25,27 @@ benchmarkSource(file = "./test_kshjhgdhsagjh.R")
 benchGetter("allprofiles")
 
 benchGetter("allbenchmarks")
+
+nrow_profiles <- nrow(benchGetter("profiles")) == 0
+assertFalse(nrow_profiles)
+benchCleaner(target = "profiles")
+nrow_profiles <- nrow(benchGetter("profiles")) == 0
+assertTrue(nrow_profiles)
+
+nrow_benchmarks <- nrow(benchGetter("benchmarks")) == 0
+assertFalse(nrow_benchmarks)
+benchCleaner(target = "benchmarks")
+nrow_benchmarks <- nrow(benchGetter("benchmarks")) == 0
+assertTrue(nrow_benchmarks)
+
+nrow_meta <- nrow(benchGetter("meta")) == 0
+assertFalse(nrow_meta)
+benchCleaner(target = "meta")
+nrow_meta <- nrow(benchGetter("meta")) == 0
+assertTrue(nrow_meta)
+
+nrow_warnings <- nrow(benchGetter("warnings")) == 0
+assertFalse(nrow_warnings)
+benchCleaner(target = "warnings")
+nrow_warnings <- nrow(benchGetter("warnings")) == 0
+assertTrue(nrow_warnings)

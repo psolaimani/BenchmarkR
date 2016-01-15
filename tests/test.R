9b8d4795e0003f46840948abe56afe77af47a9d4
Parham Solaimani <parham@bedatadriven.com>
2016-01-15 08:01:08 +0100

    extend unit test

--- tests/test.R
+++ tests/test.R
@@ -8,40 +8,30 @@ head(dt)[1,1]
 tail(dt)[1,1]
 cat("end 1st data read\n")
 
-for(i in 1:200){
-  rnorm(200)
+for(i in 1:100){
+  rnorm(100)
 }
 
-cat("2nd data read\n")
 dt <- read.table(system.file("extdata", "RNAseq2.txt", package = "benchmarkR"))
 head(dt)[1,1]
 tail(dt)[1,1]
-cat("end 2nd data read\n")
 
-cat("3rd data read\n")
 dt <- read.csv(system.file("extdata", "RNAseq2.txt", package = "benchmarkR"),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
-cat("end 3rd data read\n")
 
-cat("4th data read\n")
 dt <- read.csv2(system.file("extdata", "RNAseq2.txt", package = "benchmarkR"),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
-cat("end 4th data read\n")
 
-cat("5th data read\n")
 dt <- utils::read.delim(system.file("extdata", "RNAseq2.txt", package = "benchmarkR"),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
-cat("end 5th data read\n")
 
-cat("6st data read\n")
 dt <- read.delim2(system.file("extdata", "RNAseq2.txt", package = "benchmarkR"),sep="\t")
 head(dt)[1,1]
 tail(dt)[1,1]
-cat("end 6st data read\n")
 
-for(i in 1:1000){
-  rnorm(500)
+for(i in 1:100){
+  rnorm(100)
 }
--- tests/test3.R
+++ tests/test3.R
@@ -1,3 +1,8 @@
 require("hamcrest")
 require("utils")
 require("parallel")
+
+
+for(i in 1:100){
+  rnorm(100)
+}

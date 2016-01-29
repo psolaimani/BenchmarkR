5c313333337d2f795f7f8e3c18f2af079707f36a
Parham Solaimani <parham@bedatadriven.com>
2016-01-29 14:16:51 +0100

    use substr instead of substring

--- R/benchGetter.R
+++ R/benchGetter.R
@@ -101,7 +101,7 @@ benchGetter <- function(target, indexCol = NULL, returnCol = NULL, selectValue =
                length_name <- attr(parsed_vector, "capture.length")[line, 3] - 1
                end_pos <- start_pos + length_name
                cat(sprintf("line[%s] \"%s\" \nstart_pos: %s, length_name: %s, end_pos: %s\n",line,string_vector[line],start_pos,length_name,end_pos))
-               substring(string_vector[line], start_pos, end_pos)
+               substr(string_vector[line], start_pos, end_pos)
              }
       )
     )

8ba4b2f3b653107b409ff1fe364e6fca35ee24f1
Parham Solaimani <parham@bedatadriven.com>
2016-02-10 10:54:45 +0100

    fix test cases to match new package structure

--- tests/example_usage.R
+++ tests/example_usage.R
@@ -24,30 +24,10 @@ benchmarkSource(file = "./test.R")
 
 benchmarkSource(file = "./test_kshjhgdhsagjh.R")
 
-benchGetter("allprofiles")
-
-benchGetter("allbenchmarks")
-
-nrow_profiles <- nrow(benchGetter("profiles")) == 0
-assertFalse(nrow_profiles)
-benchCleaner(target = "profiles")
-nrow_profiles <- nrow(benchGetter("profiles")) == 0
-assertTrue(nrow_profiles)
+benchGetter("benchmarks")
 
 nrow_benchmarks <- nrow(benchGetter("benchmarks")) == 0
 assertFalse(nrow_benchmarks)
-benchCleaner(target = "benchmarks")
-nrow_benchmarks <- nrow(benchGetter("benchmarks")) == 0
-assertTrue(nrow_benchmarks)
 
 nrow_meta <- nrow(benchGetter("meta")) == 0
 assertFalse(nrow_meta)
-benchCleaner(target = "meta")
-nrow_meta <- nrow(benchGetter("meta")) == 0
-assertTrue(nrow_meta)
-
-nrow_warnings <- nrow(benchGetter("warnings")) == 0
-assertFalse(nrow_warnings)
-benchCleaner(target = "warnings")
-nrow_warnings <- nrow(benchGetter("warnings")) == 0
-assertTrue(nrow_warnings)
--- tests/test2.R
+++ tests/test2.R
@@ -3,16 +3,11 @@ require("benchmarkR")
 
 assertThat(class(BenchEnv), equalTo("environment"))
 assertThat(class(ExEnv), equalTo("environment"))
-assertThat(class(benchGetter(target = 'profiles')), equalTo("data.frame"))
 assertThat(class(benchGetter(target = 'benchmarks')), equalTo("data.frame"))
 assertThat(class(benchGetter(target = 'meta')), equalTo("data.frame"))
-assertThat(class(benchGetter(target = 'warnings')), equalTo("data.frame"))
 
-
-assertThat(ncol(benchGetter(target = 'profiles')), equalTo(7))
-assertThat(ncol(benchGetter(target = 'benchmarks')), equalTo(4))
+assertThat(ncol(benchGetter(target = 'benchmarks')), equalTo(8))
 assertThat(ncol(benchGetter(target = 'meta')), equalTo(3))
-assertThat(ncol(benchGetter(target = 'warnings')), equalTo(3))
 
 addP <- addProfiler()
 assertTrue(is.null(addP))
@@ -27,15 +22,6 @@ dt2 <- factorsAsStrings(dt)
 assertThat(class(dt$a), equalTo("factor"))
 assertThat(class(dt2$a), equalTo("character"))
 
-pkg_installation <- installUsedPackages()
-assertTrue(is.null(pkg_installation))
-pkg_installation <- installUsedPackages(file = "./file/doesntExist.R")
-assertTrue(is.null(pkg_installation))
-pkg_installation <- installUsedPackages(file = "./test.R")
-assertTrue(is.null(pkg_installation))
-pkg_installation <- installUsedPackages(file = "./test3.R")
-assertFalse(is.null(pkg_installation))
-
 replicate(3, benchmarkSource(file = "./test3.R") )
 sysId <- benchGetter(target = "systemId")
 sysId_prf <- benchGetter(target = "profile", indexCol = "process", selectValue = "BENCHMARK", returnCol = "systemId")[1]
@@ -46,37 +32,21 @@ getterReturn <- benchGetter(target = "profile")
 assertTrue(is.null(getterReturn))
 getterReturn <- benchGetter(target = "profilerun")
 assertTrue(is.null(getterReturn))
-getterReturn <- class(benchGetter(target = "warnings"))
-assertThat(getterReturn, equalTo("data.frame"))
-getterReturn <- benchGetter(target = "usedpackages")
-assertTrue(is.null(getterReturn))
-getterReturn <- benchGetter(target = "usedpackages", file ="./test.R")
-assertTrue(is.null(getterReturn))
-getterReturn <- benchGetter(target = "usedpackages", file ="./jahkjad/kjasdksd.R")
-assertTrue(is.null(getterReturn))
 
 sysId = setSystemID()
 assertFalse(is.null(sysId))
-benchCleaner(target = "meta")
-assertTrue(nrow(benchGetter(target = "meta")) == 0)
 
 old_sysId <- benchGetter(target = "systemid")
-assertTrue(nchar(old_sysId) == 18)
-assign("systemId", 666, envir = benchmarkR::ExEnv)
+assertTrue(nchar(old_sysId) == 32)
+assign("systemId", 666, envir = benchmarkR::.BenchEnv)
 setSystemID()
 new_sysId <- benchGetter(target = "systemid")
 assertTrue(new_sysId != 666)
 
-assertThat(nrow(utils::installed.packages()[,c(1,3)]), 
-           equalTo(nrow(benchGetter(target = "allpackageversions"))))
-
-nopkgs <- benchGetter( target = "usedpackages", file = "./test4.R")
-assertTrue(is.null(nopkgs))
-
-
 update <- benchDBReport()
 assertTrue(is.null(update))
-update <- benchDBReport(usr = "usr", psw = "psw", con_str = "jdbc:mysql:location/database/table")
-assertThat(update, equalTo("DB_UPDATED"))
-update <- benchDBReport(usr = "usr", psw = "psw", con_str = "jdbc:other:location/database/table")
+update <- benchDBReport(usr = "dbuser", pwd = "dbpassword", host_address="localhost", 
+                        conn_string=NULL, db_name = "benchmarkR", con_type = "mysql")
+isSame <- update == "DB_UPDATED"
+assertFalse(isSame)
 assertTrue(is.null(update))

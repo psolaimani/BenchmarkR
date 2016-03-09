b3359117f8054f85fb3a2fdcffd4a6f976f9193e
Parham Solaimani <parham@bedatadriven.com>
2016-03-09 13:11:05 +0100

    benchDBReport: use switch instead of if-statements

--- R/benchDBReport.R
+++ R/benchDBReport.R
@@ -8,112 +8,80 @@
 #' @param con_type type of database (currently only mysql supported)
 #' @import RMySQL RSQLite
 #' @export
-benchDBReport <- function(usr=NULL, pwd=NULL, 
-                          host_address=NULL, 
-                          conn_string=NULL, 
-                          db_name=NULL, con_type=NULL){
-  
-  # check if con info is provided
-  if(is.null(host_address) & is.null(conn_string) & is.null(db_name)){
-    warning("Can't write results to database. Missing connection information.\n")
-    return(NULL)
-  }
-  
-  if (con_type != "sqlite"){
-    if(is.null(usr) | is.null(pwd) | is.null(db_name) )
-      warning("Can't write results to database. \nMissing username, password, and db_name.\n")
-    return(NULL)
-  }
-  
+benchDBReport <- function(         usr = NULL, 
+                                   pwd = NULL, 
+                          host_address = NULL, 
+                           conn_string = NULL, 
+                               db_name = NULL, 
+                              con_type = c("mysql","sqlite")) {
   # Get benchmarking/profiling data
-  cur_bmrk <- benchGetter( target = "benchmarks" )
-  cur_meta <- benchGetter( target = "meta" )
+  cur_bmrk <- benchGetter(target = "benchmarks")
+  cur_meta <- benchGetter(target = "meta")
   
-  # No connection to database has been made
+  # Initiation database choice and connection state
+  con_type <- tolower(con_type)
+  con_type <- match.arg(con_type)
   CONNECTED <- FALSE
   
+  conn <- switch(
+    con_type,
+    "mysql" = {
+      # Connect to MySQL
+      if (is.null(usr) | is.null(pwd) | is.null(db_name)) {
+        warning("Missing username, password, or db_name.")
+      } if (is.null(c(host_address, conn_string, db_name))) {
+        warning("Missing connection information.")
+      }
+      
+      cat("Loading RMySQL package.\n")
+      require(RMySQL)
+      
+      cat("Connecting to MySQL database.\n")
+      db <- try(conn <- dbConnect(RMySQL(), username = usr, password = pwd, conn_string), TRUE)
+      if (inherits(db, "try-error")) {
+        db <- try(conn <- dbConnect(MySQL(), username = usr, password = pwd, host = host_address), TRUE)
+        if (inherits( db, "try-error")) {
+          warning("MySQL() and RMySQL() resulted in error!")
+        }
+      }
+      
+      conn
+    },
+    
+    "sqlite" = {
+      # Connect to SQLite
+      if (is.null(db_name)) {
+        warning("SQLite database location not provided.")
+      }
+      
+      cat("Loading RSQLite package.\n")
+      require(RSQLite)
+      
+      cat("Connecting to SQLite database.\n")
+      db <- try(conn <- dbConnect(RSQLite(), dbname = db_name), TRUE)
+      if (inherits(db, "try-error")) {
+        db <- try(conn <- dbConnect(SQLite(), dbname = db_name), TRUE)
+        if (inherits(db, "try-error")) {
+          warning("MySQL() and RMySQL() resulted in error!")
+        }
+      }
+      
+      conn
+    }
+    )
   
-  if (con_type == "mysql"){
-    # Connect to MySQL
-    cat("Connecting to MySQL database.\n")
-    require(RMySQL)
-    
-    try(
-      conn <- dbConnect(RMySQL(), username=usr, password=pwd, conn_string)
-      ,TRUE)
-    
-    if (!exists("conn")) {
-      try(conn <- dbConnect(MySQL(), username=usr, password=pwd, host=host_address),TRUE)
-    }
-    
-    if (!exists("conn")) {
-      warning("ERROR: Could not connect to database.")
-      CONNECTED <- FALSE
-      return(NULL)
-    }
-    
-    CONNECTED <- TRUE
-    cat("Connection to database established.\n")
-    
-  } else if (con_type == "sqlite") {
-    if (is.null(db_name)){
-      warning("ERROR: SQLite db location not provided.")
-      return(NULL)
-    }
-    # Connect to SQLite
-    cat("Connecting to SQLite database.\n")
-    require(RSQLite)
-    
-    try(conn <- dbConnect(RSQLite(), dbname = db_name),TRUE)
-    
-    if (!exists("conn")) {
-      try(conn <- dbConnect(SQLite(), dbname = db_name),TRUE)
-    }
-    
-    if (!exists("conn")) {
-      warning("ERROR: Could not connect to database!")
-      CONNECTED <- FALSE
-      return(NULL)
-    }
-    
-    CONNECTED <- TRUE
-    cat("Connection to database established.\n")
-#    
-#  } else if (con_type == "postgresql") {
-#    # Connect to PostgreSQL
-#    cat("Connecting to PostgreSQL database.\n")
-#    require(RPostgreSQL)
-#    conn <- dbConnect(RPostgreSQL(), con_str, usr, pwd)
-#    CONNECTED <- TRUE
-#    cat("Connection to database established.\n")
-#    
-#  } else if (con_type == "monetdb") {
-#    # Connect to MonetDB
-#    cat("Connecting to MonetDB database.\n")
-#    require(MonetDB.R)
-#    conn <- dbConnect(MonetDB.R(), username=usr, password=pwd, host=host_address, dbname=db_name)
-#    CONNECTED <- TRUE
-#    cat("Connection to database established.\n")
-#    
-#  } else if (con_type == "oracle"){
-#    # Connect to Oracle
-#    cat("Connecting to Oracle database.\n")
-#    require(ROracle)
-#    conn <- dbConnect(ROracle(), url=con_str, username=usr, password=pwd)
-#    CONNECTED <- TRUE
-#    cat("Connection to database established.\n")
-#    
-  } else {
-    
-    warning(sprintf("Database of type %s is not yet supported", con_type))
+  cat("Checking database connection\n")
+  if (!exists("conn")) {
+    warning("ERROR: Could not connect to database!")
     CONNECTED <- FALSE
-    return(NULL)
-    
+  } else {
+    CONNECTED <- TRUE
+    cat("Connection to database established!\n") 
   }
   
-  if (CONNECTED == TRUE) {
+  if (CONNECTED) {
     
-    if (con_type == "sqlite"){ 
+    if (con_type == "sqlite") { 
       # adapted from http://www.inside-r.org/packages/cran/RSQLite/docs/dbSendPreparedQuery
       bulk_insert <- function(sql, key_counts) { 
         dbBegin(conn)
@@ -123,21 +91,27 @@ benchDBReport <- function(usr=NULL, pwd=NULL,
       
       cat("Start adding records to BENCHMARKS table\n")
       benchmark_sql <- "INSERT INTO BENCHMARKS VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-      try( bulk_insert(benchmark_sql, cur_bmrk), TRUE )
+      updated <- try(bulk_insert( benchmark_sql, cur_bmrk ), TRUE)
+      if (inherits(updated, "try-error")){
+        warning("Error occured during writing benchmarks to SQLite database!")
+      } else {
+        cat("Database benchmarks table updated!")
+      }
       
       cat("Start adding records to META table\n")
       meta_sql <- "INSERT INTO META VALUES (?, ?, ?)"
-      try( bulk_insert(meta_sql, cur_meta), TRUE )
-      
-      cat("Database updated!\nClosing SQLite database connection\n")
-      
-      dbDisconnect(conn)
+      updated <- try(bulk_insert(meta_sql, cur_meta), TRUE)
+      if (inherits(updated, "try-error")){
+        warning("Error occured during writing meta to SQLite database!")
+      } else {
+        cat("Database meta table updated!")
+      }
       
     } else {
       
       cat("Start adding records to BENCHMARKS table\n")
-      for (i in 1:nrow(cur_bmrk)){
-        try(
+      for (i in 1:nrow(cur_bmrk)) {
+        updated <- try(
           dbSendQuery(conn,
                       sprintf(
                         "INSERT INTO BENCHMARKS VALUES 
@@ -149,27 +123,34 @@ benchDBReport <- function(usr=NULL, pwd=NULL,
                         cur_bmrk[i,7], cur_bmrk[i,8], 
                         cur_bmrk[i,8]
                       )
-          )
-          , TRUE)
+          ), TRUE)
+        if (inherits(updated, "try-error" )){
+          warning("Error occured during writing benchmarks to database!")
+        } else {
+          cat("Database benchmarks table updated!")
+        }
       }
       
       cat("Start adding records to META table\n")
       for (i in 1:nrow(cur_meta)){
         try(
-          dbSendQuery(conn,
+          updated <- dbSendQuery(conn,
                       sprintf(
                         "INSERT INTO META VALUES 
                        ( \'%s\', \'%s\', \'%s\' );",
                         cur_meta[i,1], cur_meta[i,2], cur_meta[i,3]
                       )
-          )
-          , TRUE)
+          ), TRUE)
+        if (inherits(updated, "try-error")){
+          warning("Error occured during writing meta to database!")
+        } else {
+          cat("Database meta table updated!")
+        }
       }
-      
-      cat("Database updated!\nClosing database connection\n")
-      
-      dbDisconnect(conn)
     }
   }
+  
+  cat("Closing SQLite database connection\n")
+  dbDisconnect(conn)
   return(invisible("DB_UPDATED"))
 }

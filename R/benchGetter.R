1792b917b2885bcfa2e937edf840d3a5d0972e18
Parham Solaimani <parham@bedatadriven.com>
2016-03-09 13:11:48 +0100

    benchGetter: use switch instead of if-statements

--- R/benchGetter.R
+++ R/benchGetter.R
@@ -8,92 +8,110 @@
 #'    \code{warnings}: returns ExecEnvironment$WARNINGS data.frame containing all warnings recorded. 
 #'    \code{systemid}: returns the unique systemId that is used to identify this system. 
 #' @param indexCol profiling records will be filtered based on content of this column
-#' @param returnCol content of this column will be returned after filtering
-#' @param selectValue this value will be compared to indexCol to filter profiling records
-#' @param selectedRunId which runId to use for filtering profiling records
+#' @param retCol content of this column will be returned after filtering
+#' @param fltVal this value will be compared to indexCol to filter profiling records
+#' @param fltRunId which runId to use for filtering profiling records
 #' @param file packages used in this file will be extracted and installed
 #' @importFrom utils installed.packages
 #' @export
-benchGetter <- function(target, indexCol = NULL, returnCol = NULL, selectValue = NULL, selectedRunId = NULL, file = NULL, runId = NULL){
-
-  target = tolower(target)
-
-  if (target == "id"){
-    # Generates a unique ID
-    exactTime <- format(Sys.time(), "%y%m%d%H%M%S")
-    randomNum <- sample(100000:999999, 1)
-    id <- as.character(paste0(exactTime,randomNum))
-    return(id)
-  }
-
-  if (target == "benchmarks"){
-    return(.BenchEnv$BENCHMARKS)
-  }
-
-  if (target == "profile"){
-    if(is.null(indexCol)  | is.null(returnCol) |  is.null(selectValue) ){
-      warning("\nRowname, columnname, or condition for subsetting Profiling data.frame is not provided.\n")
-      return(NULL)
-    }
-    return(.BenchEnv$BENCHMARKS[.BenchEnv$BENCHMARKS[indexCol] == selectValue, returnCol])
-  }
-
-  if(target == "profilerun"){
-    if(is.null(selectedRunId)){
-      warning("\nNo or empty selectedRunId provided for calculating the running time.\n")
-      return(NULL)
-    }
+benchGetter <- function(target=c("id","benchmarks","profile","profilerun",
+                                  "systemid","runid","file","runs", "timed_fun"
+                                  "bench_version","meta","computetime"), 
+                         idxCol = NULL, retCol = NULL, 
+                         fltVal = NULL, fltRunId = NULL, 
+                         file = NULL, runId = NULL) {
+  
+  target <- match.arg(target)
+  
+  result <- switch(
+    target,
+    "id" = {
+      # Generates a unique ID
+      exactTime <- format(Sys.time(), "%y%m%d%H%M%S")
+      randomNum <- sample(100000:999999, 1)
+      id <- as.character(paste0(exactTime,randomNum))
+      id
+    },
     
-    select_run <- .BenchEnv$BENCHMARKS[,grep('runId',colnames(.BenchEnv$BENCHMARKS))] == selectedRunId
-    run <- .BenchEnv$BENCHMARKS[select_run,]
-    return(run)
-  }
-  
-  if (target == "systemid"){
-    return(.BenchEnv$systemId)
-  }
-  
-  if (target == "runid"){
-    return(.BenchEnv$runId)
-  }
-  
-  if (target == "file"){
-    return(.BenchEnv$file)
-  }
-  
-  if (target == "runs"){
-    return(.BenchEnv$runs)
-  }
-  
-  if (target == "bench_version") {
-    bench_version <- Sys.getenv("SHA1")[[1]]
-    if(bench_version == "") {
-      if (is.null(file)){
-        warning("Can't get version: no file or SHA1 env variable provided.\n")
-        return(NULL)
+    "benchmarks" = {
+      get("BM", envir = .BenchEnv, mode = "list", inherits = FALSE) 
+    },
+    
+    "profile" = {
+      if(is.null(idxCol)  | is.null(retCol) |  is.null(fltVal) ) {
+        warning("Rowname, columnname, or condition for 
+                subsetting Profiling data frame is not provided.")
+        res <- NULL
+      } else {
+        res <- get("BM", envir = .BenchEnv, mode = "list", inherits = FALSE)
+        res <- res[res[idxCol] == fltVal, retCol]
       }
-      bench_version <- tools::md5sum(file)
-    }
-    return(bench_version)
-  }
-  
-  if (target == "meta"){
-    return(.BenchEnv$META)
-  }
-  
-  if (target == "computetime") {
-    if(is.null(runId)){
-      warning("\nNo or empty runId provided for calculating the running time.\n")
-      return(NULL)
-    }
-    # returns running time script minus running time reading/writing data for a given runId
-    Profile <- benchGetter( target = "profilerun", selectedRunId = runId )
-    incl <- Profile[, grep("process", colnames(Profile) ) ] == "BENCHMARK"
-    excl <- Profile[, grep("process", colnames(Profile) ) ] != "BENCHMARK"
-    time_include <- sum( as.numeric(Profile[ incl,]$duration ) )
-    time_exclude <- sum( as.numeric( Profile [ excl,]$duration ) )
-    runTime <- time_include - time_exclude
-    return( runTime )
+      res
+    },
     
-  }
+    "profilerun" = {
+      if(is.null(fltRunId)) {
+        warning("No or empty selectedRunId provided for calculating the running time.")
+        res <- NULL
+      } else {
+        flt_run <- .BenchEnv$BM[,grep('runId',colnames(.BenchEnv$BM))] == fltRunId
+        run <- .BenchEnv$BM[flt_run,]
+        res <- run
+      }
+      res
+    },
+    
+    "systemid" = {
+      get( "systemId", envir = .BenchEnv, mode = "character", inherits = FALSE )
+    },
+    
+    "runid" = {
+      get( "runId", envir = .BenchEnv, mode = "character", inherits = FALSE )
+    },
+    
+    "timed_fun" = {
+      get( "timed_fun", envir = .BenchEnv, mode = "list", inherits = FALSE )
+    },
+    
+    "file" = {
+      get( "file", envir = .BenchEnv, mode = "character", inherits = FALSE )
+    },
+    
+    "runs" = {
+      get( "runs", envir = .BenchEnv, mode = "numeric", inherits = FALSE )
+    },
+    
+    "bench_version" = {
+      bench_version <- Sys.getenv( "SHA1" )[[1]]
+      if (bench_version == "") {
+        if (is.null(file)){
+          warning("Can't get version: no file or SHA1 env variable provided.\n")
+          bench_version <- NULL
+        } else {
+          bench_version <- tools::md5sum(file)
+        }
+      }
+      bench_version
+    },
+    
+    "meta" = {
+      get("META", envir = .BenchEnv, mode = "list", inherits = FALSE)
+    },
+    
+    "computetime" = {
+      if(is.null(runId)){
+        warning("\nNo or empty runId provided for calculating the running time.\n")
+        runTime <- NULL
+      } else {
+        # returns running time script minus running time reading/writing data for a given runId
+        Profile <- benchGetter( target = "profilerun", fltRunId = runId )
+        incl <- Profile[, grep("process", colnames(Profile) ) ] == "BENCHMARK"
+        excl <- Profile[, grep("process", colnames(Profile) ) ] != "BENCHMARK"
+        time_include <- sum( as.numeric(Profile[ incl,]$duration ) )
+        time_exclude <- sum( as.numeric( Profile [ excl,]$duration ) )
+        runTime <- time_include - time_exclude
+      }
+      runTime
+    }
+  )
 }
